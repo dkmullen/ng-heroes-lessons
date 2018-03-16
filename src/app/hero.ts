b5133162395455dc54648fb45b224d8a5157f317
Dennis Mullen <dkmullen@gmail.com>
2018-03-15 21:41:55 -0400

    Thru les 4: List and detail views added

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,3 +1,5 @@
+// This file creates the Hero class, and must contain all the hero attributes
+
 export class Hero {
   id: number;
   name: string;
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero'
-
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+ 
 @Component({
-// selector is the CSS selector for this component
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
+ 
+  heroes = HEROES;
+ 
+  selectedHero: Hero;
+ 
+ 
   constructor() { }
-
-  // a Lifecycle hook - https://angular.io/guide/lifecycle-hooks#oninit
-  // A good place for init logic
+ 
   ngOnInit() {
   }
-
+ 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
